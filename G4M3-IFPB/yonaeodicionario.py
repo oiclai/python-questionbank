ef5511e03f0b54bf89085ddf54c0adb00ecb0974
oiclai <claradealcantara285@gmail.com>
2024-12-26 00:23:22 -0300

    :sparkles: feat: add new features

--- G4M3-IFPB/yonaeodicionario.py
+++ G4M3-IFPB/yonaeodicionario.py
@@ -1,13 +1,44 @@
 linhas, m = list(map(int, input().split()))
-texto = []; questoezinhas = []
-for i in range(linhas):
-    palavras = input().split()
-    texto.append(palavras)
+texto = [input().split() for _ in range(linhas)] # pra n ter q declarar uma lista vazia
 qtd_questoes = int(input())
-for j in range(qtd_questoes):
-    questoes = input()
-    questoezinhas.append(questoes)
-# ------------------SAIDA---------------------
-for x in range(len(texto)):
-    if texto[x] in questoezinhas:
-        
+questoezinhas = [input() for _ in range(qtd_questoes)] # me familiarizar c list comprehesion
+for questao in questoezinhas:
+    for i in range(len(texto)):
+        if questao in texto[i]:
+            print(f"{questao} {i + 1}")
+            break # codigo otimizado -> n precisa percorrer td matriz dps de ter encontrado
+
+
+
+
+
+
+
+
+
+'''
+----------------------- Entrada 1 ---------------------------
+10 3
+alga aram boro
+caju coce cume
+devo digo ergo
+fole frio goma
+ieis irei iris
+miro mofe momo
+note odia paro
+poem safo seda
+suco tope uiva
+unta veto vida
+5
+goma
+devo
+suco
+paro
+veto
+-------------------- Saida 1 -----------------------
+goma 4
+devo 3
+suco 9
+paro 7
+veto 10
+'''
